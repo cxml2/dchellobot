035432de561646b1d70bb8b007b61e6078e388bf
hello1234316 <85028651+hello1234316@users.noreply.github.com>
2021-06-20 17:14:45 +0800

    index.js

---
+++ index.js
@@ -0,0 +1,326 @@
+/* Setup
+
+   1. Create a .env file (click add file then remane it to .env)
+
+   2. Put "token=" (without quotes) into the .env file followed by your Discord Bot token (No spaces!)
+
+*/
+
+
+const express = require('express');
+const app = express();
+
+require('http').createServer((req, res) => res.end('Bot is alive!')).listen(3000);
+
+app.use('/ping', (req, res) => {
+  res.send(new Date());
+});
+
+process.on('uncaughtException', err => {
+  client.channels.cache.get('852851048430305321').send('There was an uncaught error', err);
+  process.exit(1); //mandatory (as per the Node.js docs)
+});
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  var hourMillseconds = 1000 * 60 * 60;
+        setInterval(function(){ // repeat this every hour
+            refresh();
+        }, hourMillseconds)
+  client.user.setActivity("for .help in " + client.guilds.size + " server\(s\)", {
+  type: "WATCHING",
+  url: "https://www.twitch.tv/catr3kd"
+});
+});
+
+function refresh(){
+  client.destroy()
+  .then(() => client.login(process.env.token));
+}
+
+var refreshMessage = "Refreshing...";
+var aitoggled = false;
+var welcometoggled = false;
+var welcomeMessage = "Welcome to the server";
+var onoff = "**Off**";
+
+client.on('message', msg => {
+  const args = msg.content.trim().split(' ');
+  const command = args.shift().toLowerCase();
+  if (aitoggled){
+    if (msg.content == "hi" || msg.content == "Hi" || msg.content == "hello" || msg.content == "Hello" || msg.content == "yo" || msg.content == "Yo" || msg.content == "hey" || msg.content == "Hey"){
+      msg.channel.send("Hey there, " + msg.author.username + "!");
+    } else if (msg.content === 'whats up' || msg.content === 'Whats up' || msg.content === 'What\'s up' || msg.content === 'what\'s up' || msg.content === 'whats up?' || msg.content === 'Whats up?' || msg.content === 'What\'s up?' || msg.content === 'what\'s up?'){
+    msg.channel.send('Not much right now.');
+    } else if (msg.content === 'gtg' || msg.content === 'Gtg'){
+    msg.channel.send(`See ya then, ` + msg.author.username + "!");
+    } else if (msg.content === 'epic' || msg.content === 'Epic' || msg.content === 'cool' || msg.content === 'Cool' || msg.content === 'awesome' || msg.content === 'Awesome' || msg.content === 'epic!' || msg.content === 'Epic!' || msg.content === 'cool!' || msg.content === 'awesome!' || msg.content === 'Awesome!'){
+    msg.channel.send("Cool!");
+    } else if (msg.content === 'lol' || msg.content === 'Lol' || msg.content === 'LOL'){
+      if (Math.random() >= 3){
+        msg.react('😄');
+      }
+    } else if (msg.content.includes("bored")){
+      msg.channel.send(">>> **Bored Menu**\n\n• Use fun commands like .ascii or .joke\n• Watch CatR3kd on twitch\n• Play a game\n• Do something other than discord");
+    } else if (msg.content === 'bruh' || msg.content === 'Bruh' || msg.content === 'BRUH'){
+       if (msg.author.username !==           "SmartBot" || Math.random <= 0.4){
+        var bruhs = ['bruh', 'BRUH', 'bruh'];
+        var bruh = Math.floor(Math.random() * bruhs.length);
+        msg.channel.send(bruhs[bruh]);
+      }
+    } else if (msg.content.includes("sad") || msg.content.includes("Sad") || msg.content.includes("bad") || msg.content.includes("Bad") || msg.content.includes("suck") || msg.content.includes("Suck") || msg.content.includes("lame") || msg.content.includes("Lame") || msg.content.includes("\)\:") || msg.content.includes("\)\;")){
+      msg.react("😥");
+    } else if (msg.content.includes("YES") || msg.content.includes("yesss")){
+      msg.channel.send("Oh yeah, baby");
+    } else if (msg.content.includes("ikr") || msg.content.includes("Ikr") || msg.content.includes("IKR")){
+      msg.channel.send("Yeah");
+    } else if (msg.content.includes("hry") || msg.content.includes("Hry")) {
+      msg.channel.send("I'm good, thanks.");
+    } else if (msg.content === "ily" || msg.content === "Ily" || msg.content === "ILY" || msg.content.includes("I love you") || msg.content.includes("i love you")){
+      msg.react("💕");
+      msg.react("🥰");
+      msg.channel.send("*Awwwww..*");
+    } else if (msg.content.includes("???") && msg.author.id !== client.user.id){
+      msg.channel.send("*???*");
+    } else if (msg.content.includes("SmartBot") || msg.content.includes("smartbot") || msg.content.includes("Smartbot") || msg.content.includes("smart bot") || msg.content.includes("Smart Bot") || msg.content.includes("Smart bot")){
+      //Stuff that goes with SmartBot goes here
+      if (msg.content.includes("poopy") || msg.content.includes("Poopy") || msg.content.includes("POOPY") || msg.content.includes("idiot") || msg.content.includes("Idiot") || msg.content.includes("IDIOT") || msg.content.includes("dumb") || msg.content.includes("Dumb") || msg.content.includes("DUMB") && !msg.content.includes("not") || !msg.content.includes("Not") || !msg.content.includes("NOT")){
+        msg.delete();
+      }
+    }
+  }
+  if (msg.content == ".help") {
+    var helpMenu = ">>> **Help Menu**\n\n__Commands:__\n\n.ai: Brings up help menu for this bot\'s integrated chatbot AI!\n\n.welcome: Brings up help menu for the welcome feature!\n\n.ascii: Sends a random ASCII artpiece!\n\n.joke: Tells a joke\n\n.bruh: Bruh\n\n.invite: Invite the bot to your server\n\n.serverstatus: Displays how many servers, users, and channels the bot is running with.\n\nthis bot was made by HELLO PHONE.";
+    if (msg.author.id === "544206793492922368"){
+      helpMenu = helpMenu + "\n\n\n__Dev commands:__\n\(*Usable by hello phone only*\)\n\n.refresh: Refresh the bot, but not the code"
+    }
+    msg.channel.send(helpMenu);
+  } else if (msg.content == ".ascii") {
+    var ascii = [`                         ⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣻⣿⡿⣫⣿⣿⠟⠁⠀⣼⠁⠀⠈⣿⣿⢿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣯⣾⢟⣩⣾⠿⠋⠁⠀⢠⣴⣧⣤⣤⣤⣿⡏⠈
+⣿⣿⢿⣿⣿⡿⠟⠽⠛⠉⠀⠚⠉⠚⠋⠉⠀⠀⠀⠤⠒⠉⠀⠀⠀⠀⢀⡿⠉⠂
+⣿⠏⢸⠿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠀⠀⠀
+⣿⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⡀⠀⠀⠀⠀
+⡇⠀⣀⣤⣤⣤⣴⣶⣶⣶⣶⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣶⣶⣤⣤⣤⣀
+⣩⣿⡿⠟⣿⣿⣿⣿⣿⣯⠙⠳⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣯⠙⠻⢿
+⠹⣿⠀⠀⣿⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⠀⠀⢈
+⡀⠈⠂⡀⣙⢩⢿⡿⠝⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠃⠻⣿⠃⠀⢀⡘
+⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠉⠉⠉⠉⠁⠰⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠂⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⢷⣶⣶⠶⠟⠋⠙⢶⣤⣴⠄⠀⠀⠀⠀⠀⠀
+⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼
+⣮⡂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿`,`
+⣇⣿⠘⣿⣿⣿⡿⡿⣟⣟⢟⢟⢝⠵⡝⣿⡿⢂⣼⣿⣷⣌⠩⡫⡻⣝⠹⢿⣿⣷
+⡆⣿⣆⠱⣝⡵⣝⢅⠙⣿⢕⢕⢕⢕⢝⣥⢒⠅⣿⣿⣿⡿⣳⣌⠪⡪⣡⢑⢝⣇
+⡆⣿⣿⣦⠹⣳⣳⣕⢅⠈⢗⢕⢕⢕⢕⢕⢈⢆⠟⠋⠉⠁⠉⠉⠁⠈⠼⢐⢕⢽
+⡗⢰⣶⣶⣦⣝⢝⢕⢕⠅⡆⢕⢕⢕⢕⢕⣴⠏⣠⡶⠛⡉⡉⡛⢶⣦⡀⠐⣕⢕
+⡝⡄⢻⢟⣿⣿⣷⣕⣕⣅⣿⣔⣕⣵⣵⣿⣿⢠⣿⢠⣮⡈⣌⠨⠅⠹⣷⡀⢱⢕
+⡝⡵⠟⠈⢀⣀⣀⡀⠉⢿⣿⣿⣿⣿⣿⣿⣿⣼⣿⢈⡋⠴⢿⡟⣡⡇⣿⡇⡀⢕
+⡝⠁⣠⣾⠟⡉⡉⡉⠻⣦⣻⣿⣿⣿⣿⣿⣿⣿⣿⣧⠸⣿⣦⣥⣿⡇⡿⣰⢗⢄
+⠁⢰⣿⡏⣴⣌⠈⣌⠡⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣉⣉⣁⣄⢖⢕⢕⢕
+⡀⢻⣿⡇⢙⠁⠴⢿⡟⣡⡆⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣵⣵⣿
+⡻⣄⣻⣿⣌⠘⢿⣷⣥⣿⠇⣿⣿⣿⣿⣿⣿⠛⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿
+⣷⢄⠻⣿⣟⠿⠦⠍⠉⣡⣾⣿⣿⣿⣿⣿⣿⢸⣿⣦⠙⣿⣿⣿⣿⣿⣿⣿⣿⠟
+⡕⡑⣑⣈⣻⢗⢟⢞⢝⣻⣿⣿⣿⣿⣿⣿⣿⠸⣿⠿⠃⣿⣿⣿⣿⣿⣿⡿⠁⣠
+⡝⡵⡈⢟⢕⢕⢕⢕⣵⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⣀⣈⠙
+⡝⡵⡕⡀⠑⠳⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⢉⡠⡲⡫⡪⡪⡣`,`
+⡆⣐⢕⢕⢕⢕⢕⢕⢕⢕⠅⢗⢕⢕⢕⢕⢕⢕⢕⠕⠕⢕⢕⢕⢕⢕⢕⢕⢕⢕ ⢐⢕⢕⢕⢕⢕⣕⢕⢕⠕⠁⢕⢕⢕⢕⢕⢕⢕⢕⠅⡄⢕⢕⢕⢕⢕⢕⢕⢕⢕ ⢕⢕⢕⢕⢕⠅⢗⢕⠕⣠⠄⣗⢕⢕⠕⢕⢕⢕⠕⢠⣿⠐⢕⢕⢕⠑⢕⢕⠵⢕ ⢕⢕⢕⢕⠁⢜⠕⢁⣴⣿⡇⢓⢕⢵⢐⢕⢕⠕⢁⣾⢿⣧⠑⢕⢕⠄⢑⢕⠅⢕ ⢕⢕⠵⢁⠔⢁⣤⣤⣶⣶⣶⡐⣕⢽⠐⢕⠕⣡⣾⣶⣶⣶⣤⡁⢓⢕⠄⢑⢅⢑ ⠍⣧⠄⣶⣾⣿⣿⣿⣿⣿⣿⣷⣔⢕⢄⢡⣾⣿⣿⣿⣿⣿⣿⣿⣦⡑⢕⢤⠱⢐ ⢠⢕⠅⣾⣿⠋⢿⣿⣿⣿⠉⣿⣿⣷⣦⣶⣽⣿⣿⠈⣿⣿⣿⣿⠏⢹⣷⣷⡅⢐ ⣔⢕⢥⢻⣿⡀⠈⠛⠛⠁⢠⣿⣿⣿⣿⣿⣿⣿⣿⡀⠈⠛⠛⠁⠄⣼⣿⣿⡇⢔ ⢕⢕⢽⢸⢟⢟⢖⢖⢤⣶⡟⢻⣿⡿⠻⣿⣿⡟⢀⣿⣦⢤⢤⢔⢞⢿⢿⣿⠁⢕ ⢕⢕⠅⣐⢕⢕⢕⢕⢕⣿⣿⡄⠛⢀⣦⠈⠛⢁⣼⣿⢗⢕⢕⢕⢕⢕⢕⡏⣘⢕ ⢕⢕⠅⢓⣕⣕⣕⣕⣵⣿⣿⣿⣾⣿⣿⣿⣿⣿⣿⣿⣷⣕⢕⢕⢕⢕⡵⢀⢕⢕ ⢑⢕⠃⡈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢃⢕⢕⢕ ⣆⢕⠄⢱⣄⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⢁⢕⢕⠕⢁`,`
+⠀⠀⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠰⡿⠿⠛⠛⠻⠿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣀⣄⡀⠀⠀⠀⠀⢀⣀⣀⣤⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢸⣿⣿⣷⠀⠀⠀⠀⠛⠛⣿⣿⣿⡛⠿⠷⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠘⠿⠿⠋⠀⠀⠀⠀⠀⠀⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣿⣷⣄⠀⢶⣶⣷⣶⣶⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣿⣿⣿⠀⠀⠀⠀⠀⠈⠙⠻⠗⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⣰⣿⣿⣿⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣠⣾⣿⣿⣿⣥⣶⣶⣿⣿⣿⣿⣿⠿⠿⠛⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠈⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠛⢿⣿⣿⣿⣿⣿⣿⡿⠟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`,`
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⣰⣿⣿⣿⣿⠿⠿⣿⣿⣿⣿⣿⣿⣿⣧⢀⠀⠀⠀⠀
+⠀⠀⠀⣿⣿⣿⠋⠀⠀⠀⠀⠀⠙⠀⠙⣿⣿⣿⣷⢳⢀⠀⠀⠀
+⠀⠀⣠⣿⣿⣿⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⢀
+⠀⠀⣸⣿⣿⣿⠸⠀⠀⠀⠒⠒⠒⠐⠀⠀⢿⣿⣿⣿⣿⣿⠀⠀
+⠀⣴⣿⣿⣿⡿⠀⠒⣋⣙⡒⢰⠀⠤⣖⠒⢾⣿⣿⣿⣿⣧⠀⠀
+⢺⣿⣿⣿⣿⢀⠀⠀⠉⠉⠉⠸⠀⡇⠉⠉⠀⢿⣿⣿⣿⣄⠀⠀
+⠀⠙⣿⣿⣧⢻⠀⠀⠀⠀⠀⠠⠀⠰⠀⠀⠀⣸⠸⣿⣿⠿⠰⠀
+⠀⠀⠀⠹⣿⣿⣿⣷⠀⡠⠙⣲⣔⣅⢡⣰⣷⣿⣿⣿⣧⠀⠀⠀
+⠀⠀⠀⣼⣿⣿⣿⣿⠀⡿⠭⠭⠭⠭⢿⠀⣿⢻⣿⣿⠃⠀⠀⠀
+⠀⠀⠀⠙⠛⣿⢻⠹⣿⠐⠙⠛⠟⠉⢀⣴⡟⢿⣿⡏⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⡟⠀⠀⠻⣦⣤⣶⠾⠋⠀⠀⠁⡦⢄⢀⠀⠀⠀
+⠀⠀⠀⠀⡠⠁⡇⠑⢄⠀⠀⠀⠀⠀⠀⠔⠀⠀⠁⠀⠀⠀⠉⠁
+⠀⠔⠊⠁⠀⠀⣇⠀⠀⠀⠑⡤⠤⢎⠁⠀⠀⡘⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⢢⠠⠀⡠⢆⠀⠀⡠⠙⢄⠀⡸⠀⠀⠀⠀⠀⠀ `,`
+⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⡏⠉⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⣿
+⣿⣿⣿⣿⣿⣿⠀⠀⠀⠈⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠛⠉⠁⠀⣿
+⣿⣿⣿⣿⣿⣿⣧⡀⠀⠀⠀⠀⠙⠿⠿⠿⠻⠿⠿⠟⠿⠛⠉⠀⠀⠀⠀⠀⣸⣿
+⣿⣿⣿⣿⣿⣿⣿⣷⣄⠀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠠⣴⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡟⠀⠀⢰⣹⡆⠀⠀⠀⠀⠀⠀⣭⣷⠀⠀⠀⠸⣿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠈⠉⠀⠀⠤⠄⠀⠀⠀⠉⠁⠀⠀⠀⠀⢿⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⢾⣿⣷⠀⠀⠀⠀⡠⠤⢄⠀⠀⠀⠠⣿⣿⣷⠀⢸⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿
+⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿`,`
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣧⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣧⠀⠀⠀⢰⡿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡟⡆⠀⠀⣿⡇⢻⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⣿⠀⢰⣿⡇⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⡄⢸⠀⢸⣿⡇⢸⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⡇⢸⡄⠸⣿⡇⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⢸⡅⠀⣿⢠⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣥⣾⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡿⡿⣿⣿⡿⡅⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⠉⠀⠉⡙⢔⠛⣟⢋⠦⢵⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣄⠀⠀⠁⣿⣯⡥⠃⠀⢳⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⡇⠀⠀⠀⠐⠠⠊⢀⠀⢸⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⡿⠀⠀⠀⠀⠀⠈⠁⠀⠀⠘⣿⣄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⣠⣿⣿⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣷⡀⠀⠀⠀
+⠀⠀⠀⠀⣾⣿⣿⣿⣿⣿⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣧⠀⠀
+⠀⠀⠀⡜⣭⠤⢍⣿⡟⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⢛⢭⣗⠀
+⠀⠀⠀⠁⠈⠀⠀⣀⠝⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠄⠠⠀⠀⠰⡅
+⠀⠀⠀⢀⠀⠀⡀⠡⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⠔⠠⡕⠀
+⠀⠀⠀⠀⣿⣷⣶⠒⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⠀⠀⠀⠀
+⠀⠀⠀⠀⠘⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠈⢿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠊⠉⢆⠀⠀⠀⠀
+⠀⢀⠤⠀⠀⢤⣤⣽⣿⣿⣦⣀⢀⡠⢤⡤⠄⠀⠒⠀⠁⠀⠀⠀⢘⠔⠀⠀⠀⠀
+⠀⠀⠀⡐⠈⠁⠈⠛⣛⠿⠟⠑⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠉⠑⠒⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`,`
+⢀⡴⠑⡄⠀⠀⠀⠀⠀⠀⠀⣀⣀⣤⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠸⡇⠀⠿⡀⠀⠀⠀⣀⡴⢿⣿⣿⣿⣿⣿⣿⣿⣷⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠑⢄⣠⠾⠁⣀⣄⡈⠙⣿⣿⣿⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⢀⡀⠁⠀⠀⠈⠙⠛⠂⠈⣿⣿⣿⣿⣿⠿⡿⢿⣆⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⢀⡾⣁⣀⠀⠴⠂⠙⣗⡀⠀⢻⣿⣿⠭⢤⣴⣦⣤⣹⠀⠀⠀⢀⢴⣶⣆ 
+⠀⠀⢀⣾⣿⣿⣿⣷⣮⣽⣾⣿⣥⣴⣿⣿⡿⢂⠔⢚⡿⢿⣿⣦⣴⣾⠁⠸⣼⡿ 
+⠀⢀⡞⠁⠙⠻⠿⠟⠉⠀⠛⢹⣿⣿⣿⣿⣿⣌⢤⣼⣿⣾⣿⡟⠉⠀⠀⠀⠀⠀ 
+⠀⣾⣷⣶⠇⠀⠀⣤⣄⣀⡀⠈⠻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠉⠈⠉⠀⠀⢦⡈⢻⣿⣿⣿⣶⣶⣶⣶⣤⣽⡹⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠉⠲⣽⡻⢿⣿⣿⣿⣿⣿⣿⣷⣜⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣷⣶⣮⣭⣽⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⣀⣀⣈⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠹⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀ 
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠻⠿⠿⠿⠿⠛⠉`]
+    msg.channel.send(ascii[(Math.random() * ascii.length) | 0]);
+  } else if (msg.content == ".toggleai"){
+      if (msg.member.hasPermission('ADMINISTRATOR')){
+        if (aitoggled){
+         aitoggled = false;
+          msg.channel.send("AI Chatbot Is Toggled **Off**");
+        } else {
+         aitoggled = true;
+         msg.channel.send("AI Chatbot Is Toggled **On**");
+        }
+      } else {
+        msg.channel.send("This command is only usable by administrators!");
+      }
+    
+  } else if (msg.content == ".ai"){
+    if (aitoggled){
+      onoff = "**On**";
+    } else {
+      onoff = "**Off**";
+    }
+    msg.channel.send(`>>> **AI Help Menu**\n\n__Usage:__\n\nWhen the Chatbot feature is toggled\, I will respond to simple messages that are\'nt commands\, such as greetings\, common questions\, and other things, to make servers more fun!\n\n__Commands:__\n\n.toggleai: Toggles the feature, only usable by administrators\n\n.checkaitoggle: Check if the AI is toggled\n\n\nAI Chatbot Is Currently Toggled ` + onoff);
+  } else if (msg.content == ".checkaitoggle"){
+    if (aitoggled){
+      msg.channel.send("AI Chatbot Is Toggled **On**");
+    } else {
+      msg.channel.send("AI Chatbot Is Toggled **Off**");
+    }
+  } else if (msg.content == ".bruh"){
+    msg.channel.send(`:blue_square: 
+:blue_square: 
+:blue_square: :blue_square: :blue_square: 
+:blue_square:               :blue_square: 
+:blue_square: :blue_square: :blue_square:
+
+:blue_square::blue_square::blue_square:
+:blue_square:      :blue_square:
+:blue_square:
+:blue_square:
+
+:blue_square:              :blue_square:
+:blue_square:              :blue_square:
+:blue_square:              :blue_square:
+:blue_square::blue_square::blue_square:
+
+:blue_square:
+:blue_square:
+:blue_square::blue_square::blue_square:
+:blue_square:             :blue_square:
+:blue_square:             :blue_square:`);
+  } else if (msg.content == ".serverstatus"){
+    msg.channel.send("Bot is running with " + client.users.size+
+" users, in  " + client.channels.size + 
+" channels of " + client.guilds.size + " server\(s\)");
+  } else if (msg.content === '.joke'){
+    var puns = ['"I have a split personality," said Tom, being frank.', 'It\'s hard to explain puns to kleptomaniacs because they always take things literally', 'England doesn\'t have a kidney bank, but it does have a Liverpool', 'All chemists know that alcohol is always a solution', 'Let me tell you about my grandfather. He was a good man, a brave man. He had the heart of a lion, and a lifetime ban from the zoo', 'A mean crook going down stairs is a condescending con, descending', 'I saw an ad for burial plots, and I thought: \“That’s the last thing I need!\”'];
+    var pun = Math.floor(Math.random() * puns.length);
+    msg.channel.send(puns[pun]);
+  } else if (msg.content === '.welcome'){
+    if (welcometoggled){
+      onoff = "**On**";
+    } else {
+      onoff = "**Off**";
+    }
+    msg.channel.send(">>> **Welcome Menu**\n\n__Usage:__\nThe welcome feature sends a welcome messages whenever a new member joins the server!\n\n__Commands:__\n\n.togglewelcome: Toggles the welcome feature, only usable by administrators\n\n.checkwelcometoggle: Check if werlcomes are toggled.\n\n.changewelcome: change server welcome message. If you need more help or info use __.changewelcomehelp__\n\n.changewelcomehelp: Brings up more info about how to use __.changewelcome__\n\n\nWelcomes are currently toggled " + onoff);
+  } else if (msg.content === '.togglewelcome'){
+    if (msg.member.hasPermission('ADMINISTRATOR')){
+      if (welcometoggled){
+        welcometoggled = true;
+        msg.channel.send("Welcomes now toggled **On**");
+      } else {
+        welcometoggled = false;
+        msg.channel.send("Welcomes now toggled **Off**");
+      }
+    } else{
+      msg.channel.send("This command is only usable by administrators!");
+    }
+  } else if (msg.content === '.checkwelcometoggle'){
+    if (welcometoggled){
+      msg.channel.send("Welcomes are currently toggled **On**");
+    } else {
+      msg.channel.send("Welcomes are currently toggled **Off**");
+    }
+  } else if (msg.content == ".changewelcomehelp"){
+    msg.channel.send(">>> **Changing Welcome Messages**\n\nTo change welcome messages use __.changewelcome \<NewWelcome\>__. This will change the underlined text with whatever you put instead of __\<NewWelcome\>__.\n\nExample:\n__" + welcomeMessage + "__, \<User\>!" );
+  } else if (command == ".changewelcome"){
+    if (!args.length) {
+		  return message.channel.send(`you did not set up to any channel, ${message.author}!`);
+	  } else {
+      welcomeMessage = args;
+      msg.channel.send("welcome massage setted up to \"**" + args + "**\".");
+    }
+  } else if (msg.content == ".info"){
+    msg.channel.send(">>> **Info**\n\nthis bot was made by  **HELLO PHONE** !");
+  } else if (msg.content == ".invite"){
+    msg.channel.send("invite link： https://discord.com/api/oauth2/authorize?client_id=856051979817189396&permissions=0&scope=bot");
+  } else if (msg.content == ".refresh" && msg.author.id === "544206793492922368"){
+    msg.channel.send(refreshMessage)
+    .then(() => client.destroy())
+    .then(() => client.login(process.env.token));
+  }
+});
+
+client.on("serverNewMember", (server, user) => {
+  if (welcometoggled == true){
+    msg.channel.send(welcomeMessage + user + "!");
+  }
+});
+
+client.login("put your token here");
